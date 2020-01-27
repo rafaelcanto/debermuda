6f49283de59da40050f319cd7a16718ab7777090
Rafael Canto <rafaelcanto@outlook.com>
2020-01-27 16:55:06 -0300

    Update VoteContext.cs

--- debermuda-api/Data/VoteContext.cs
+++ debermuda-api/Data/VoteContext.cs
@@ -12,7 +12,7 @@ namespace DeBermudaApi.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                @"Server=tcp:debermuda-dbsrv01.database.windows.net,1433;Initial Catalog=db01;Persist Security Info=False;User ID=dbadmin;Password=s3nh4Segur@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                @"Server=tcp:debermuda-dbsrv01.database.windows.net,1433;Initial Catalog=db01;Persist Security Info=False;User ID=dbadmin;Password=******;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
     }
 }
